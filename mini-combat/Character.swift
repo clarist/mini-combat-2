f8bb19c2e5d0fc6148cb67ee784849a217087e9e
Clare Hallock <clarist@gmail.com>
2016-03-31 22:52:17 -0500

    First pass

---
+++ mini-combat/Character.swift
@@ -0,0 +1,70 @@
+//
+//  Character.swift
+//  mini-combat
+//
+//  Created by Clare Hallock on 3/30/16.
+//  Copyright © 2016 Clare Hallock. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    private var _hitPoints: Int
+    private var _attackPower: Int
+    private var _name: String
+    private var _combatStatus: String
+
+    var hitPoints: Int {
+        get {
+            return _hitPoints
+        }
+
+    }
+    
+    var attackPower: Int {
+        get {
+            return _attackPower
+        }
+    }
+    
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    var combatStatus: String {
+        get {
+            return _combatStatus
+        }
+    }
+    
+    init(startingHP: Int, startingPower: Int, name: String) {
+        self._hitPoints = startingHP
+        self._attackPower = startingPower
+        self._name = name
+        self._combatStatus = ""
+        
+    }
+    
+    func takeDamage(damageTaken: Int) {
+        self._hitPoints = self._hitPoints - damageTaken
+    }
+    
+    func tryAttack(target: Character, attackStrength: Int, hitRoll: Int) {
+        if self.hitPoints > 0 {
+            if target.hitPoints > 0 {
+                if hitRoll > target.attackPower {
+                    target.takeDamage(attackStrength)
+                    _combatStatus = "\(target.name) takes \(attackStrength) damage! (\(target.hitPoints) remain)"
+                } else {
+                    _combatStatus = "\(self.name) has missed!"
+                }
+            } else {
+                _combatStatus = "\(target.name) is dead. :("
+            }
+        } else {
+                self._combatStatus = "\(self.name) is dead and can't attack!"
+            }
+        }
+}
--- mini-combat/ViewController.swift
+++ mini-combat/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var player: Character!
+    var monster: Character!
+
+    
+    @IBOutlet weak var blueLabelText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        player = Character(startingHP: 20, startingPower: 5, name: "Player")
+        monster = Character(startingHP: 15, startingPower: 6, name: "Monster")
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onMonsterButtonPressed(sender: UIButton) {
+        if monster.hitPoints > 0 {
+            let d20 = Int(arc4random_uniform(20))
+            let d6 = Int(arc4random_uniform(6))
+            
+            monster.tryAttack(player, attackStrength: d6, hitRoll: d20)
+            blueLabelText.text = monster.combatStatus
+        } else {
+            blueLabelText.text = monster.combatStatus
+        }
+    }
+    
+    @IBAction func onPlayerButtonPressed(sender: UIButton) {
+        let d20 = Int(arc4random_uniform(20))
+        let d6 = Int(arc4random_uniform(6))
+        
+        player.tryAttack(monster, attackStrength: d6, hitRoll: d20)
+        blueLabelText.text = player.combatStatus
+        }
+            
+//            let attackStrength = Int(arc4random_uniform(6))
+//            let hitRoll = Int(arc4random_uniform(20))
+//            
+//            if hitRoll > monster.attackPower {
+//                monster.takeDamage(attackStrength)
+//                blueLabelText.text = "Monster takes \(attackStrength) damage! (\(monster.hitPoints) remain)"
+//            } else {
+//                blueLabelText.text = "Player has missed!"
+//            }
+//        } else {
+//            blueLabelText.text = "Monster is dead. :("
+//        }
 
+    
+//    func disableButton() {
+//        NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: Selector("onPlayerButtonPressed"), userInfo: nil, repeats: false)
+//        
+//    }
+//}
 
 }
 
